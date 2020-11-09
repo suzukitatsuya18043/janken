646e2dc7aca07b16c3796dd77ddf75220163561b
suzukitatsuya18043 <e1b18043@oit.ac.jp>
2020-11-09 19:37:09 +0900

    セキュリティの認証処理を実装する

---
+++ src/main/java/oit/is/z0968/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -0,0 +1,52 @@
+package oit.is.z0968.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  /**
+   * 誰がログインできるか(認証処理)
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    // $ sshrun htpasswd -nbBC 10 Tatsuya Sigebu717
+    auth.inMemoryAuthentication().withUser("Tatsuya")
+        .password("$2y$10$OTilyNhXu1SlHPh5zGr.Bea0ou7OYU8AUPZwazvY9FgoDZP.zvvFu").roles("USER");
+    // $ sshrun htpasswd -nbBC 10 admin Admin
+    auth.inMemoryAuthentication().withUser("admin").password("$2y$10$6yVW.Ct9xNjJah4LBgXYsucT3x8Ba/lEF1pAieei9URujzmISdgLK").roles("USER");
+
+    // 開発中は↓の書き方でも良いが，平文でパスワードが保存される
+    // auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("pAssw0rd")).roles("USER");
+    // auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("pAssw0rd")).roles("ADMIN");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * 認証されたユーザがどこにアクセスできるか（認可処理）
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    // Spring Securityのフォームを利用してログインを行う
+    http.formLogin();
+
+    http.authorizeRequests().antMatchers("/lec02/**").authenticated();
+
+    http.logout().logoutSuccessUrl("/");
+  }
+
+}
